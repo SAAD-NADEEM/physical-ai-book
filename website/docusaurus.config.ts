7c36185afecf9c75123bf29f8fa701f76eee8844
SAAD-NADEEM <saadngaming99090@gmail.com>
2025-12-22 19:10:50 +0500

    fixed base url for vercel deploy.

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   url: 'https://Hackathone01withqwen.github.io', // Replace with your base URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/hackathoneWithQWEN_V2/', // Adjust based on your repository name
+  baseUrl: '/', // Adjust based on your repository name
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
